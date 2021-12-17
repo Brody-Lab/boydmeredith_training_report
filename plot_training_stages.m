bea51039a02d72b606fa898f4fdbfd94c3758d5b
tyler <jboydmeredith@gmail.com>
2021-12-17 17:37:12 -0500

    update plot training stages

--- plot_training_stages.m
+++ plot_training_stages.m
@@ -2,7 +2,7 @@ function plot_training_stages(ratname, start_date, varargin)
 p = inputParser;
 addParameter(p, 'fignum', 1);
 addParameter(p, 'protocols', {'ProAnti3', 'PBups', 'PBupsWT', []});
-addParameter(p, 'list_stages', 0);
+addParameter(p, 'list_stages', 1);
 parse(p, varargin{:});
 par = p.Results;
 fn = par.fignum;
@@ -28,12 +28,13 @@ datenums = res.datenums;
 
 first_date =  datenums(find(~isnan(datenums),1));
 datex = datenums - first_date;
-
+%%
 fh = figure(fn); clf
-set(fh, 'position', [2,7, 9, 4])
+set(fh, 'position', [12,7, 9, 4])
 
 if par.list_stages
     ax = subplot(1,5,1:4);
+    axpos = get(ax,'position');
 else
     ax = axes;
 end
@@ -46,17 +47,60 @@ start_datestr = datestr(first_date,'mm-dd-yy');
 xlabel(ax,sprintf('days from %s',start_datestr))
 
 set(ax, 'ytick', [0:20], 'ylim', [min(stagenums) max(stagenums)]+[-.25 .25])
-title(sprintf('%s', ratname))
+title(ax,sprintf('%s', ratname))
 box(ax,'off')
 
 not_nan = find(~isnan(res.protnums));
 not_nan_prot_changes = find(diff(res.protnums(not_nan))~=0);
 pre_changes = not_nan(not_nan_prot_changes);
 post_changes = not_nan(not_nan_prot_changes+1);
-res.prots(pre_changes);
-res.prots(post_changes);
-plot(datex(pre_changes).*[1 1], ylim,'color',[1 1 1].*.5,'linewidth',2)
+
+plot(ax,repmat(datex(pre_changes),1,2)', ...
+    ylim,'color',[1 .65 .85],'linewidth',2)
+%%
+datenums_nonans = datenums(1) + (0:length(datenums)-1);
+newmonth = find(day(datenums_nonans)==1);
+temp = month(datenums_nonans(newmonth));
+monthnames = string(datestr(datenums_nonans(newmonth),'mmm'))
+
+%%
+xt=2:2:length(monthnames)
+monthnames(xt)
+%%
+set(ax,'XMinorTick','on','xtick',newmonth(xt),'xticklabel',monthnames(xt))
+ax.XAxis.MinorTickValues=newmonth;
+
+%set(ax,'xtick','xtick',[0:30:
 %ismember(res.prots, protocols)
 %cellfun(protocols)
+%%
+pre_prots = res.prots(pre_changes)
+post_prots = res.prots(post_changes)
 
+%%
+if par.list_stages
+unique_prots = unique(res.protnums(~isnan(res.protnums)));
+inc = 0;
+text_inc = -1;
+for pp = 1:length(unique_prots)
+    this_prot = res.protnums == unique_prots(pp);
+    this_protname = res.prots(find(this_prot,1));
+
+    unique_stages = unique(res.stagenums(this_prot));
+    text(ax,max(get(ax,'xlim'))*1.05, max(get(ax,'ylim')) - text_inc*.5,...
+        sprintf('--- %s ---',this_protname{1}));
+    for ss = 1:length(unique_stages);
+        text_inc = text_inc+1;
+        inc = inc + 1;
+        this_stage = unique_stages(ss);
+        
+        this_stagename = res.stagenames{find(this_prot & ...
+            res.stagenums==this_stage,1)};
+        stagenames{inc} = this_stagename;
+        text(ax,max(get(ax,'xlim'))*1.05, max(get(ax,'ylim')) - text_inc*.5, this_stagename);
+    end
+    text_inc = text_inc+1;
+end
+
+end
 
