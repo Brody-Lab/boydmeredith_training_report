02be315d3ab638276a35e604484c45918802b220
tyler <jboydmeredith@gmail.com>
2021-12-17 15:05:10 -0500

    add code to number protocols and find change days

--- get_training_stages.m
+++ get_training_stages.m
@@ -1,4 +1,24 @@
-function [stagenums, datenums] = get_training_stages(ratname, start_date, varargin)
+function res = get_training_stages(ratname, start_date, varargin)
+p = inputParser;
+addParameter(p, 'protocols', {'ProAnti3', 'PBups', 'PBupsWT'})
+parse(p,varargin{:})
+
+if isempty(start_date)
+   sessdates = bdata(['select sessiondate from sessions'...
+       ' where ratname="{S}"'], ratname);
+   start_date = sessdates{1};
+end
+
+savedir = '~/projects/rat_training/';
+savepath = fullfile(savedir, [ratname '_stages.mat']);
+if exist(savepath, 'file')
+    start_date_in = start_date;
+    load(savepath)
+    start_date = res.start_date;
+    if datenum(start_date_in) == datenum(start_date)
+        return;
+    end
+end
 
 % build file list 
 settings_dir = '/Volumes/brody/RATTER/SoloData/Settings/';
@@ -13,15 +33,18 @@ the_date  = start_datenum;
 datenums = nan(ndays,1);
 stagenums = nan(ndays,1);
 maxstages = nan(ndays,1);
+stagenames = cell(ndays,1);
 prots = cell(ndays,1);
+protnums = nan(ndays,1);
 
 for dd = 1:ndays
     if mod(dd,10)==1,
         fprintf('\nworking on day %i of %i\n', dd, ndays);
     end
     [settings, stageName, ~, endStageName, nStages, prot,...
-    start_t, end_t] = getStageName(ratname,the_date);
+        start_t, end_t] = getStageName(ratname,the_date);
     the_date = the_date + 1;
+    
     if ~isempty(settings)
         temp = regexp(stageName, '\d+','match');
         stagenum = str2num(temp{1});
@@ -30,11 +53,19 @@ for dd = 1:ndays
         stagemax = str2num(temp{1});
         maxstages(dd) = stagemax;
         datenums(dd) = the_date;
+        prot = strjoin(regexp(prot, '\w', 'match'),'');
         prots{dd} = prot;
+        protnums(dd) = find(ismember(p.Results.protocols,prot));
+        stagenames{dd} = stageName;
+    else
+        prots{dd} = '';
     end
     
 end
-    
-
-    
-    
+res.start_date = start_date;
+res.stagenums = stagenums;
+res.stagenames = stagenames;
+res.datenums = datenums;
+res.prots = prots;
+res.protnums = protnums;
+save(savepath, 'res')
--- plot_training_stages.m
+++ plot_training_stages.m
@@ -1,10 +1,30 @@
 function plot_training_stages(ratname, start_date, varargin)
-[stagenums, datenums] = get_training_stages(ratname, start_date);
+p = inputParser;
+addParameter(p, 'fignum', 1);
+parse(p, varargin{:});
+par = p.Results;
+fn = par.fignum;
+
+if nargin < 2
+    start_date = [];
+end
+
+if iscell(ratname)
+    nrats = length(ratname);
+    for rr = 1:nrats
+        this_ratname = ratname{rr};
+        plot_training_stages(this_ratname, start_date, 'fignum',rr);
+    end
+end
+
+res = get_training_stages(ratname, start_date);
+stagenums = res.stagenums;
+datenums = res.datenums;
+
 first_date =  datenums(find(~isnan(datenums),1));
 datex = datenums - first_date;
 
-%%
-figure(1); clf
+figure(fn); clf
  ax = axes;
 plot(ax,datex(~isnan(datex)), stagenums(~isnan(datex)), 'k:', 'linewidth', 1.75)
 hold(ax,'on')
@@ -16,3 +36,18 @@ xlabel(ax,sprintf('days from %s',start_datestr))
 
 set(ax, 'ytick', [0:20], 'ylim', [min(stagenums) max(stagenums)]+[-.25 .25])
 title(sprintf('%s', ratname))
+box(ax,'off')
+%%
+protocols = {'ProAnti3', 'PBups', 'PBupsWT', []};
+
+not_nan = find(~isnan(res.protnums));
+not_nan_prot_changes = find(diff(res.protnums(not_nan))~=0);
+pre_changes = not_nan(not_nan_prot_changes);
+post_changes = not_nan(not_nan_prot_changes+1);
+res.prots(prot_changes)
+res.prots(not_nan(not_nan_prot_changes+1))
+plot(datex(pre_changes).*[1 1], ylim,'k','linewidth',2)
+%ismember(res.prots, protocols)
+%cellfun(protocols)
+
+
