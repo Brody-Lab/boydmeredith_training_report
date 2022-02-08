1a63322b2eb83393696d48c6dcea3ced9624900b
tyler <jboydmeredith@gmail.com>
2022-02-08 16:54:22 -0500

    update get ratnames

--- get_ratnames.m
+++ get_ratnames.m
@@ -1,30 +1,45 @@
-function [ratnames, experimenter] = get_ratnames
-
-j_rats_2019 = {'J243', 'J244', 'J246', 'J247', 'J248','J249', 'J250',...
-    'J300', 'J301', 'J302','J303', 'J306','J307','J308',...
-    'J309','J310','J311','J312','J313','J314','J315'};
-    
-j_rats_2020 = cellfun(@(x) sprintf('J%i',x), num2cell(316:323),'uniformoutput',0);
-h_rats  = {'H153', 'H161','H126','H138','H170','H171','H173',...
-    'H209','H203','H215','H194'};
-ratnames = {j_rats_2019{:} j_rats_2020{:} h_rats{:}};
-
-ratnames = { 'J324','J325','J327','J328','J330','J332','J333',...
-    'J334','J335','J336','J337', 'J338', 'J339', 'J340', ...
-    'J341','J342', 'J343', 'J344', 'J345', 'J346', 'J347', 'J348',...
-    'J349', 'J350'};
-
+function [ratnames, experimenter] = get_ratnames(group)
+if nargin < 1
+    group = 'active'
+end
+switch group
+    case 'wt_all'
+           ratnames = {'J243', 'J244', 'J246', 'J247', 'J248', 'J250',...
+               'J300', 'J301', 'J303', 'J304', 'J305', 'J313', 'J314', ...
+               'J315', 'J330', 'J332', 'Z159', 'Z160', 'Z161', 'Z162',...
+               'Z163', 'Z164', 'Z165', 'Z166', 'Z253', 'Z254', 'Z255',...
+               'Z256', 'Z257', 'Z258'};
+    case 'wt_muscimol'
+        ratnames = {'Z255', 'J244'};
+    case 'active'
+        ratnames = {'J324','J325','J327','J328','J330','J332',...
+            'J335','J337', 'J338', 'J339', 'J340', ...
+            'J341','J342', 'J343', 'J344', 'J345', 'J346', 'J348',...
+            'J349', 'J350'};
+end
+% 
+% j_rats_2019 = {'J243', 'J244', 'J246', 'J247', 'J248','J249', 'J250',...
+%     'J300', 'J301', 'J302','J303', 'J306','J307','J308',...
+%     'J309','J310','J311','J312','J313','J314','J315'};
+%     
+% j_rats_2020 = cellfun(@(x) sprintf('J%i',x), num2cell(316:323),'uniformoutput',0);
+% h_rats  = {'H153', 'H161','H126','H138','H170','H171','H173',...
+%     'H209','H203','H215','H194'};
+% ratnames = {j_rats_2019{:} j_rats_2020{:} h_rats{:}};
+% 
+% 
+% 
 experimenter = ratnames;
 for rr = 1:length(ratnames)
-    if ratnames{rr}(1)=='J'
+    if ratnames{rr}(1)=='J' | ratnames{rr}(1)=='Z'
         experimenter{rr} = 'Tyler';
-    else
+    elseif ratnames{rr}(1)=='H'
         experimenter{rr} = 'Ahmed';
     end
 end
-    
-    
-    dead = {'Z159','Z160','Z161','Z163','Z164','Z165','J245'};
+%     
+%     
+%     dead = {'Z159','Z160','Z161','Z163','Z164','Z165','J245'};
     %
 % 
 % alive = {'Z159'  'Z160'  'Z161'  'Z162'  'Z163'  'Z164'  'Z166'};
