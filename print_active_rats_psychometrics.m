ef7a8074cf0da33e418f1e28b9a481a10bc3bba6
tyler <jboydmeredith@gmail.com>
2022-09-24 21:08:03 -0400

    add print_active_rats_psychometrics

--- print_active_rats_psychometrics.m
+++ print_active_rats_psychometrics.m
@@ -1,6 +1,12 @@
-function fh = print_active_rats_psychometrics(ratnames)
+function fh = print_active_rats_psychometrics(ratnames, ndays)
+wp = setup_waiting_path;
+fig_path = wp.fig_dir;
+
 if nargin < 1
     ratnames = get_ratnames;
+end
+if nargin < 2
+    ndays = 7;
 end
 
 set(0,'defaultfigureunits','inches')
@@ -11,23 +17,18 @@ nrats = length(ratnames);
 fht = 15;
 %fx = mp(1)+mp(1,3)/2-fw/2;
 warning('off')
-ndays = 7;
 nrows = 5;
 ncols = ceil(nrats/nrows);
 fw    = 3.5*ncols;
 fx    = sum(mp(1,[1 3])) - fw - 1;
 %
-
-set(f , 'position',   [fx 1  fw fht], 'units','inches')
-
+fh = figure(1); clf
+set(fh , 'position',   [fx 1  fw fht], 'units','inches')
 for rr = 1:nrats
     ax = subplot(nrows,ncols,rr);
-
-    %%
     plotPsychometricRat(ratnames{rr},...
-        'daterange',ndays,'type','delta','ax',ax);
+        'daterange',ndays,'type','delta','ax',ax,'plotLastDay',true);
     drawnow
-
 end
 warning('on')
 print(f, fullfile(fig_path, ['rat_psycho_' datestr(today, 'mmddyy')]), '-dpng')
