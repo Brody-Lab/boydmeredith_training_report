8746f7b4c6fb0af541d856fb93a5d4a0f6145955
tyler <jboydmeredith@gmail.com>
2022-09-24 21:07:37 -0400

    improve training_report

--- training_report.m
+++ training_report.m
@@ -1,4 +1,7 @@
-function res = training_report(ratnames, startDate, endDate, fh)
+function res = training_report(ndays)
+% function res = training_report(ratnames, startDate, endDate, fh)
+%
+% 
 
 if isempty(bdata)
     bdata('connect')
@@ -6,50 +9,16 @@ end
 
 fig_path = '~/projects/rat_training';
 
+fh = figure(1); clf
 
-if nargin < 4
-    fh = uifigure(1); clf
-end
-if nargin < 3
-    endDate = [];
-end 
-if nargin < 2
-    startDate = [];
-end
 if nargin < 1 | isempty(ratnames)
     ratnames = get_ratnames;
 end
 
 %[fh res] = plot_training_report(ratnames, startDate, endDate, fh)
-print_training_report(ratnames, endDate, 10);
-%%
-f = figure(1); clf
-set(0,'units','inches')
-mp      = get(0, 'MonitorPositions');
-max_x   = mp(1,1) + mp(1,3);
-%%
-nrats = length(ratnames);
-fht = 15;
-%fx = mp(1)+mp(1,3)/2-fw/2;
-warning('off')
-ndays = 7;
-nrows = 5;
-ncols = ceil(nrats/nrows);
-fw    = 3.5*ncols;
-fx    = sum(mp(1,[1 3])) - fw - 1;
-%
+ndays_to_check = 10;
+print_training_report(ratnames, endDate, ndays_to_check);
+ndays_to_plot = 7;
+print_active_rats_psychometrics(ratnames, ndays_to_plot);
 
-set(f , 'position',   [fx 1  fw fht])
-%%
-for rr = 1:nrats
-    ax = subplot(nrows,ncols,rr);
-    try
-        plotPsychometricRat(ratnames{rr},...
-            'daterange',ndays,'type','delta','ax',ax);
-        drawnow
-    catch
-    end
-end
-warning('on')
-print(f, fullfile(fig_path, ['rat_psycho_' datestr(today, 'mmddyy')]), '-dpng')
 
