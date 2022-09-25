8fcc6e3b969c0ea5666f049fb1fabe521a729c23
tyler <jboydmeredith@gmail.com>
2022-09-25 18:41:21 -0400

    fix training_report inputs

--- training_report.m
+++ training_report.m
@@ -1,5 +1,5 @@
-function res = training_report(ndays)
-% function res = training_report(ratnames, startDate, endDate, fh)
+function training_report()
+% function training_report()
 %
 % 
 
@@ -9,16 +9,15 @@ end
 
 fig_path = '~/projects/rat_training';
 
-fh = figure(1); clf
-
 if nargin < 1 | isempty(ratnames)
     ratnames = get_ratnames;
 end
 
-%[fh res] = plot_training_report(ratnames, startDate, endDate, fh)
 ndays_to_check = 10;
-print_training_report(ratnames, endDate, ndays_to_check);
 ndays_to_plot = 7;
-print_active_rats_psychometrics(ratnames, ndays_to_plot);
+check_date = datestr(today,29);
+
+print_training_report(ratnames, check_date, ndays_to_check);
+fh = print_active_rats_psychometrics(ratnames, ndays_to_plot);
 
 
